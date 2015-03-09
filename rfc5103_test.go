429f82d9999199c3361ff46e1217b4c7257d689d
Jakob Borg <jakob@nym.se>
2015-03-09 20:52:24 +0100

    Support RFC5103 Bidirectional Flow Export (fixes #1)

---
+++ rfc5103_test.go
@@ -0,0 +1,32 @@
+package ipfix
+
+import "testing"
+
+func TestRFC5103(t *testing.T) {
+	f := builtinDictionary[dictionaryKey{29305, 239}]
+	if f.Name != "biflowDirection" {
+		t.Errorf("Incorrect name for biflowDirection in %+v", f)
+	}
+	if f.FieldID != 239 {
+		t.Errorf("Incorrect field ID in %+v", f)
+	}
+	if f.EnterpriseID != 29305 {
+		t.Errorf("Incorrect reverse PEN in %+v", f)
+	}
+
+	f = builtinDictionary[dictionaryKey{29305, 85}]
+	if f.Name != "reverseOctetTotalCount" {
+		t.Errorf("Incorrect name for reverseOctetTotalCount in %+v", f)
+	}
+	if f.FieldID != 85 {
+		t.Errorf("Incorrect field ID in %+v", f)
+	}
+	if f.EnterpriseID != 29305 {
+		t.Errorf("Incorrect reverse PEN in %+v", f)
+	}
+
+	_, ok := builtinDictionary[dictionaryKey{29305, 173}]
+	if ok {
+		t.Errorf("Incorrect existence of non-reversible flowKeyIndicator")
+	}
+}
