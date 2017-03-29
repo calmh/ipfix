42e56d1314151a8adc5def15f02c3211be4cff09
Jakob Borg <jakob@kastelo.net>
2017-03-29 13:34:37 +0200

    Fix race condition on templates access

--- interpreter.go
+++ interpreter.go
@@ -146,7 +146,9 @@ func (i *Interpreter) Interpret(rec DataRecord) []InterpretedField {
 // InterpretInto interprets a raw DataRecord into an existing slice of
 // InterpretedFields. If the slice is not long enough it will be reallocated.
 func (i *Interpreter) InterpretInto(rec DataRecord, fieldList []InterpretedField) []InterpretedField {
+	i.session.mut.RLock()
 	tpl := i.session.templates[rec.TemplateID]
+	i.session.mut.RUnlock()
 	if tpl == nil {
 		return nil
 	}
